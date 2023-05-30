0d0c27f4c53f7102d4cd98af0f80c4aae7c81429
Mikkel <Mikkel123sej@gmail.com>
2023-05-30 21:36:03 +0200

    finished anagrams thingy

--- anagrams/src/main.rs
+++ anagrams/src/main.rs
@@ -1,52 +1,57 @@
+use std::collections::HashMap;
+use std::time::SystemTime;
 
 fn main() {
-    let input = include_str!("wordlist.txt").to_ascii_lowercase();
+    let start_time = SystemTime::now();
+
+    let input = include_str!("wordlist.txt");
     let word_list: Vec<&str> = input.lines().collect();
 
-    for word in &word_list {
-        if word.contains("'s") {
-            continue;
+    // create hashmap with keys as anagram connections (letters), values as all words with those letters
+    let word_map = {
+        let mut word_map: HashMap<String, Vec<&str>> = HashMap::new();
+        for word in word_list {
+            // dbg!(&word);
+            let word_sorted = {
+                let mut word_chars: Vec<char> = word.chars().collect();
+                word_chars.sort();
+                String::from_iter(word_chars)
+            };
+            // dbg!(&word_sorted);
+            if word_map.contains_key(&word_sorted) {
+                if !word_map.get(&word_sorted).unwrap().contains(&word) {
+                    word_map.get_mut(&word_sorted).unwrap().push(word);
+                    // println!("Added.");
+                }
+            } else {
+                word_map.insert(word_sorted.clone(), vec![word]);
+                // println!("Created.");
+            }
         }
-
-        let mut anagrams: Vec<String> = Vec::new();
-
-        let word_chars: Vec<char> = word.chars().collect();
-        let possible_word_matches: Vec<&str> = word_list.iter().filter(|word_in_list| word_in_list.len() == word.len() ).copied().collect();
-        for valid_combination in char_combinations(Vec::new(), word_chars, possible_word_matches) {
-            dbg!(&valid_combination);
-            anagrams.push(valid_combination);
-        }
-        println!("{} has {} anagrams: {:?}", word, anagrams.len(), anagrams);
-    }
-}
-
-fn char_combinations(mut selected_chars: Vec<char>, remaining_chars: Vec<char>, matching_valid_words: Vec<&str>) -> Vec<String> {
-    if remaining_chars.len() == 1 {
-        selected_chars.push(*remaining_chars.first().unwrap());
-        let final_word: String = selected_chars.into_iter().collect();
-        if matching_valid_words.contains(&final_word.as_str()) {
-            return vec![final_word];
-        }
-        return vec![];
-    }
+        word_map
+    };
+    println!("Stored all the words as a hashmap.");
     
-    let mut words: Vec<String> = Vec::new();
-    for selected_char in &remaining_chars {
-        let mut selected_chars_for_selection = selected_chars.clone();
-        selected_chars_for_selection.push(*selected_char);
-        
-        let word_so_far: String = selected_chars_for_selection.iter().collect();
-        let remaining_matching_words: Vec<&str> = matching_valid_words.iter().filter(|valid_word| valid_word.contains(&word_so_far)).copied().collect();
-        if remaining_chars.is_empty() {
-            continue;
-        }
-        
-        let mut remaining_chars_for_selection = remaining_chars.clone();
-        remaining_chars_for_selection.retain(|c| c != selected_char);
-        
-        let mut new_words = char_combinations(selected_chars_for_selection, remaining_chars_for_selection, remaining_matching_words);
-        words.append(&mut new_words);
-    }
+    let anagram_map: HashMap<String, Vec<&str>> = word_map.into_iter().filter(|(_, words)| words.len() > 1).collect();
+    println!("Created anagram hashmap from word hashmap.");
+    
+    println!("Anagram hashmap contains {} keys, with {} values in total.", anagram_map.keys().len(), anagram_map.values().map(|words| words.len()).sum::<usize>());
+    
+    let all_anagrams: Vec<(&String, &Vec<&str>)> = anagram_map.iter().collect();
 
-    words
+    let largest_anagram_words = {
+        let mut by_word_len = all_anagrams.clone();
+        by_word_len.sort_by(|a, b| b.0.len().cmp(&a.0.len()));
+        *by_word_len.first().unwrap()
+    };
+    println!("Largest anagram by word length ({} characters): {}", largest_anagram_words.0.len(), largest_anagram_words.1.join(", "));
+    
+    let largest_anagram_amount = {
+        let mut by_word_len = all_anagrams.clone();
+        by_word_len.sort_by(|a, b| b.1.len().cmp(&a.1.len()));
+        *by_word_len.first().unwrap()
+    };
+    println!("Largest anagram by word amount ({} entries): {}", largest_anagram_amount.1.len(), largest_anagram_amount.1.join(", "));
+
+    println!("\nProgram took {} ms to finish.", SystemTime::duration_since(&SystemTime::now(), start_time).unwrap().as_millis());
 }
