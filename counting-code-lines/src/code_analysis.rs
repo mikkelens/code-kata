4f359b1c01f846a7283154faca3b0bdfcc5c32de
Mikkel <Mikkel123sej@gmail.com>
2023-06-06 11:23:05 +0200

    made it faster

--- counting-code-lines/src/code_analysis.rs
+++ counting-code-lines/src/code_analysis.rs
@@ -6,7 +6,8 @@
 const WHITE_SPACE: char = ' ';
 const COMMENT_OUTER: char = '/';
 const COMMENT_MULTI_INNER: char = '*';
-const STRING_LITERAL_EDGES: char = '\"';
+const STR_LITERAL_EDGES: char = '\"';
+const CHAR_EDGES: char = '\'';
 #[derive(PartialEq, Debug)]
 enum ReadState {
     OnWhiteSpace, // effective reset, base value
@@ -14,7 +15,7 @@ enum ReadState {
     OnIdentifiedSymbol, // continue search in case of string literal
     InEndOfLineComment, // basically a card to skip to next line
     InMultilineComment { ending_comment: bool }, // potentially "least valuable", can continue on multiple lines
-    InStringLiteral, // "most valuable", can potentially continue on multiple lines
+    InQuotation(char), // "most valuable", can potentially continue on multiple lines
 }
 impl ReadState {
     fn after_next_char_in_line(&self, next: char) -> ReadState {
@@ -22,19 +23,19 @@ impl ReadState {
             Self::OnWhiteSpace => match next {
                 WHITE_SPACE => Self::OnWhiteSpace, // '  '
                 COMMENT_OUTER => Self::OnSlash, // ' /'
-                STRING_LITERAL_EDGES => Self::InStringLiteral, // ' "'
+                STR_LITERAL_EDGES | CHAR_EDGES => Self::InQuotation(next), // ' "'
                 _ => Self::OnIdentifiedSymbol // ' X', could also be ' *'
             },
             Self::OnSlash => match next {
                 WHITE_SPACE => Self::OnWhiteSpace, // '/_', might as well be identified symbol technically?
                 COMMENT_OUTER => Self::InEndOfLineComment, // '//'
                 COMMENT_MULTI_INNER => Self::InMultilineComment { ending_comment: false }, // '/*'                
-                STRING_LITERAL_EDGES => Self::InStringLiteral, // '/"'
+                STR_LITERAL_EDGES | CHAR_EDGES => Self::InQuotation(next), // '/"'
                 _ => Self::OnIdentifiedSymbol // '/X'
             },
             Self::OnIdentifiedSymbol => match next {
                 COMMENT_OUTER => Self::OnSlash, // 'X/'
-                STRING_LITERAL_EDGES => Self::InStringLiteral, // 'X"'
+                STR_LITERAL_EDGES | CHAR_EDGES => Self::InQuotation(next), // 'X"'
                 _ => Self::OnIdentifiedSymbol // ' X', could also be '_
             },
             Self::InEndOfLineComment => unreachable!(), // '/!', should never stay in this state
@@ -42,29 +43,28 @@ impl ReadState {
                 COMMENT_OUTER if ending_comment => Self::OnWhiteSpace, // '*/', use whitespace as reset
                 c => Self::InMultilineComment { ending_comment: c == COMMENT_MULTI_INNER } // ' *' (true) or ' @' (false)
             },
-            Self::InStringLiteral => {
-                if next == STRING_LITERAL_EDGES { // characters do not matter if not closing quote '"'
+            Self::InQuotation(boundary) => {
+                if next == boundary { // characters do not matter if not closing quote '"'
                     Self::OnIdentifiedSymbol // '@"', still counts as symbol
                 } else {
-                    Self::InStringLiteral // '@@'
+                    Self::InQuotation(boundary) // '@@', keep boundary in mind
                 }
             },
         }
     }
     fn is_multi_line(&self) -> bool {
-        matches!(self, ReadState::InMultilineComment { ending_comment: _ } | ReadState::InStringLiteral)
+        matches!(self, ReadState::InMultilineComment { ending_comment: _ } | ReadState::InQuotation(_))
     }
 }
 
-#[allow(unused)]
-fn count_valid_code_lines(literal: &str) -> u32 {
-    println!();
+pub fn count_valid_code_lines(literal: &str) -> u32 {
+    // println!();
 
     let mut lines_with_symbol = 0;
     let mut prev_state = ReadState::OnWhiteSpace;
 
     for line_with_all_whitespace in literal.trim().lines() {
-        println!("\nBeginning line with state {:?}:\n{:?}", prev_state, line_with_all_whitespace);
+        // println!("\nBeginning line with state {:?}:\n{:?}", prev_state, line_with_all_whitespace);
 
         if !prev_state.is_multi_line() {
             prev_state = ReadState::OnWhiteSpace;
@@ -81,7 +81,7 @@ fn count_valid_code_lines(literal: &str) -> u32 {
                 break;
             }
             
-            let new_is_counted = new_state == ReadState::OnIdentifiedSymbol || new_state == ReadState::InStringLiteral;
+            let new_is_counted = matches!(new_state, ReadState::OnIdentifiedSymbol | ReadState::InQuotation(_));
             let prev_is_counted = prev_state == ReadState::OnSlash && new_state == ReadState::OnWhiteSpace;
             if new_is_counted || prev_is_counted {
                 symbol_in_line = true;
@@ -97,9 +97,9 @@ fn count_valid_code_lines(literal: &str) -> u32 {
 
         if symbol_in_line {
             lines_with_symbol += 1;
-            println!("Line had symbol. Total lines with symbols so far: {lines_with_symbol}");
+            // println!("Line had symbol. Total lines with symbols so far: {lines_with_symbol}");
         } else {
-            println!("Line had no symbol.");
+            // println!("Line had no symbol.");
         }
     }
 
@@ -108,23 +108,19 @@ fn count_valid_code_lines(literal: &str) -> u32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::*;
+    use crate::code_analysis::*;
 
     const EXAMPLE_1: &str = include_str!("test_examples\\Dave.java");
     const EXAMPLE_1_OUT: u32 = 3;
     #[test]
     fn example_1() {
-        println!();
         assert_eq!(count_valid_code_lines(EXAMPLE_1), EXAMPLE_1_OUT);
-        println!("\n");
     }
     
     const EXAMPLE_2: &str = include_str!("test_examples\\Hello.java");
     const EXAMPLE_2_OUT: u32 = 5;
     #[test]
     fn example_2() {
-        println!();
         assert_eq!(count_valid_code_lines(EXAMPLE_2), EXAMPLE_2_OUT);
-        println!("\n");
     }
 }
--- counting-code-lines/src/main.rs
+++ counting-code-lines/src/main.rs
@@ -1,19 +1,184 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::env;
+use std::fs;
+use std::path::PathBuf;
+use phf::phf_map;
 
 pub mod code_analysis;
 
-fn main() {
-    let dir = env::current_dir().unwrap();
-    println!("DIR: {:?}", dir);
-
-    let all_args: Vec<String> = env::args().collect();
-    let arg_str = all_args.join(", ");
-    println!("ARGS: {}", arg_str);
-
-    let mut arg_iter = all_args.into_iter().skip(1); // skip executable path
-    let Some(target_str) = arg_iter.next() else {
-        panic!("Did not provide a target.")
+struct CommandInfo {
+    // name: &'static str,
+    env_var: &'static str,
+    description: &'static str
+}
+const IGNORE_DIR_KEY: &str = "ignore_dirs";
+const FILE_TYPE_KEY: &str = "file_types";
+static COMMANDS: phf::Map<&'static str, CommandInfo> = phf_map! {
+    "ignore_dirs" => CommandInfo { /*name: IGNORE_DIR_KEY,*/ env_var: "GLOBAL_IGNORED_DIRS", description: "list of directory names to ignore" },
+    "file_types" => CommandInfo { /*name: FILE_TYPE_KEY,*/ env_var: "GLOBAL_FILE_TYPES", description: "list of file type extensions to search for" },
+};
+    
+const ENV_VAR_SPLIT: char = ';';
+fn load_command_env_variables(command: &CommandInfo) -> Option<Vec<String>> {
+    let command_var = env::var(command.env_var);
+    let Ok(var_str) = command_var else {
+        eprintln!("Could not find {} using enviromental variable '{}'", command.description, command.env_var);
+        return None;
     };
-    println!("Target: {target_str}");
-    // let targets = 
+    let loaded: Vec<String> = var_str.split(ENV_VAR_SPLIT).map(|value| value.trim().to_string()).collect();
+    println!("Loaded environmental variables: [{}]", loaded.join(", "));
+    Some(loaded)
+}
+
+fn main() {
+    let dir = env::current_dir().expect("User had no environment to get directory from");
+    let all_args: Vec<String> = env::args().skip(1).collect(); // skip executable path, then collect
+    println!("All args: {}", all_args.join(", "));
+
+    let mut command_settings_map: HashMap<&'static str, Vec<String>> = HashMap::from([
+        (IGNORE_DIR_KEY, load_command_env_variables(COMMANDS.get(IGNORE_DIR_KEY).unwrap()).unwrap_or(vec![])),
+        (FILE_TYPE_KEY, load_command_env_variables(COMMANDS.get(FILE_TYPE_KEY).unwrap()).unwrap_or(vec![]))
+    ]);
+    
+    let mut total_lines = 0;
+    'arg_loop: for arg in all_args.iter() {
+        const LOCAL_COMMAND_ARG: &str = "--";
+        const LOCAL_COMMAND_DIVIDER: char = '=';
+        let command_trim = arg.trim_start_matches(LOCAL_COMMAND_ARG);
+        let is_command = command_trim != arg;
+        if is_command {
+            let command_str = command_trim.to_lowercase();
+            let command_split = command_str.split_once(LOCAL_COMMAND_DIVIDER);
+            let Some((command_type_str, command_val_str)) = command_split else {
+                eprintln!("Tried parsing argument '{}' as command, failed to see assignment (=).", arg);
+                continue;
+            };
+            
+            for (name, info) in COMMANDS.entries() {
+                if command_type_str.contains(name) {
+                    command_settings_map.get_mut(name).unwrap().push(command_val_str.to_string());
+                    println!("Added {} values '{}' to {} settings: [{}]", 
+                        name, command_val_str, info.description, command_settings_map.get(name).unwrap().join(", "));
+                    continue 'arg_loop;
+                }
+            } 
+            
+            eprintln!("Did not understand command '{}' (value [{}]).", command_type_str, command_val_str);
+
+        } else {            
+            let arg_path = dir.join(arg);
+            let ignored_dirs = command_settings_map.get(IGNORE_DIR_KEY).unwrap().clone();
+            let file_types = command_settings_map.get(FILE_TYPE_KEY).unwrap().clone();
+            let i_d_map: HashSet<String> = HashSet::from_iter(ignored_dirs);
+            let f_t_map: HashSet<String> = HashSet::from_iter(file_types);
+            total_lines += count_lines_path_recursive(&arg_path, &i_d_map, &f_t_map);
+        }
+    }
+    println!("Program finished. Total lines counted: {}", total_lines);
+}
+
+fn count_lines_path_recursive(path: &PathBuf, ignored_dirs: &HashSet<String>, file_types: &HashSet<String>) -> u128 {
+    if path.is_dir() {
+        let dir_path = path;
+        if let Some(dir_name_os_str) = dir_path.file_name() {
+            if let Some(dir_name) = dir_name_os_str.to_str() {
+                if ignored_dirs.contains(&dir_name.to_string()) {
+                    // println!("Ignoring directory '{}'...", dir_name);
+                    return 0;
+                }
+            }
+        }
+        let Ok(paths) = fs::read_dir(dir_path) else {
+            eprintln!("{} could not be read as a directory.", dir_path.display());
+            return 0;
+        };
+        
+        let mut lines = 0;
+        for dir_entry_fallible in paths {
+            let Ok(dir_entry) = dir_entry_fallible else {
+                eprintln!("Could not use dir_entry: {}", dir_entry_fallible.unwrap_err());
+                continue;
+            };
+            let path = dir_entry.path();
+            lines += count_lines_path_recursive(&path, ignored_dirs, file_types);
+        }
+        return lines;
+    } else if path.is_file() {
+        let file_path = path;
+        if !file_types.is_empty() {
+            if let Some(file_extension_os_str) = file_path.extension() {
+                if let Some(file_extension) = file_extension_os_str.to_str() {
+                    if file_types.contains(&file_extension.to_string()) {
+                        // println!("Ignoring file extension '{}'...", file_extension);
+                        return count_lines_in_file(file_path);
+                    }
+                }
+            }
+        } else {
+            return count_lines_in_file(file_path);
+        }
+    }
+    0
+}
+
+#[allow(unused)]
+fn analyze_path_recursive(path: &PathBuf, ignored_dirs: &Vec<String>, file_types: &Vec<String>) {
+    if path.is_dir() {
+        let dir_path = path;
+        if let Some(dir_name_os_str) = dir_path.file_name() {
+            if let Some(dir_name) = dir_name_os_str.to_str() {
+                if ignored_dirs.contains(&dir_name.to_string().to_lowercase()) {
+                    // println!("Ignoring directory '{}'...", dir_name);
+                    return;                    
+                }
+            }
+        }
+        let Ok(paths) = fs::read_dir(dir_path) else {
+            eprintln!("{} could not be read as a directory.", dir_path.display());
+            return;
+        };
+        
+        for dir_entry_fallible in paths {
+            let Ok(dir_entry) = dir_entry_fallible else {
+                eprintln!("Could not use dir_entry: {}", dir_entry_fallible.unwrap_err());
+                continue;
+            };
+            let path = dir_entry.path();
+            analyze_path_recursive(&path, ignored_dirs, file_types);
+        }
+    } else if path.is_file() {
+        let file_path = path;
+        if !file_types.is_empty() {
+            if let Some(file_extension_os_str) = file_path.extension() {
+                if let Some(file_extension) = file_extension_os_str.to_str() {
+                    if file_types.contains(&file_extension.to_string()) {
+                        // println!("Ignoring file extension '{}'...", file_extension);
+                        analyze_file(file_path);
+                    }
+                }
+            }
+        } else {
+            analyze_file(file_path);
+        }
+    }
+}
+
+fn count_lines_in_file(file_path: &PathBuf) -> u128 {
+    let Ok(file_str) = fs::read_to_string(file_path) else {
+        eprintln!("{:?} could not be read as a file to a string.", file_path);
+        return 0;
+    };
+    code_analysis::count_valid_code_lines(&file_str) as u128
+}
+
+fn analyze_file(file_path: &PathBuf) {
+    let Ok(file_str) = fs::read_to_string(file_path) else {
+        eprintln!("{:?} could not be read as a file to a string.", file_path);
+        return;
+    };
+    
+    let name = file_path.file_name().unwrap(); // should be valid because we read it as a file
+    let lines_of_code = code_analysis::count_valid_code_lines(&file_str);
+    println!("{name:?} has {lines_of_code} lines of code.")
 }
